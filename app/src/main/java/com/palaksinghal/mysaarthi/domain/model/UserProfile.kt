3baa0cfba95b523551f28a7d94ac6f2896dfa6a0
palaksinghal14 <palaksinghal148@gmail.com>
2026-08-20 17:35:43 +0530

    feat: add UserProfile domain model and UserProfileRepo interface for onboarding feature

---
+++ app/src/main/java/com/palaksinghal/mysaarthi/domain/model/UserProfile.kt
@@ -0,0 +1,11 @@
+package com.palaksinghal.mysaarthi.domain.model
+
+data class UserProfile(
+    val uid: String = "",
+    val displayName: String = "",
+    val practices: List<String> = emptyList(),
+    val howLongOnPath: String = "",
+    val isOpenToSatsang: Boolean = false,
+    val spiritualIntro: String = "",
+    val onboardingCompleted: Boolean = false
+)
---
+++ app/src/main/java/com/palaksinghal/mysaarthi/domain/repository/UserProfileRepo.kt
@@ -0,0 +1,10 @@
+package com.palaksinghal.mysaarthi.domain.repository
+
+import com.palaksinghal.mysaarthi.domain.model.UserProfile
+
+interface UserProfileRepo {
+
+    suspend fun saveUserProfile(userProfile: UserProfile) :Result<Unit>
+    suspend fun getUserProfile(uid:String) :Result<UserProfile>
+
+}
