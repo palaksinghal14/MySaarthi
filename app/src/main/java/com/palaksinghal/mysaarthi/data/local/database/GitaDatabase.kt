df9053e20485c2d4570a94a72a70725a8b758f39
palaksinghal14 <palaksinghal148@gmail.com>
2026-09-05 17:40:52 +0530

    chore: update GitaDatabase to version 2  — add UserProfileEntity and SadhanaEntryEntity

--- app/src/main/java/com/palaksinghal/mysaarthi/data/local/database/GitaDatabase.kt
+++ app/src/main/java/com/palaksinghal/mysaarthi/data/local/database/GitaDatabase.kt
@@ -2,10 +2,22 @@ package com.palaksinghal.mysaarthi.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.palaksinghal.mysaarthi.data.local.converters.Converters
+import com.palaksinghal.mysaarthi.data.local.dao.SadhanaDao
 import com.palaksinghal.mysaarthi.data.local.dao.ShlokaDao
+import com.palaksinghal.mysaarthi.data.local.dao.UserProfileDao
+import com.palaksinghal.mysaarthi.data.local.entity.SadhanaEntryEntity
 import com.palaksinghal.mysaarthi.data.local.entity.ShlokaEntity
+import com.palaksinghal.mysaarthi.data.local.entity.UserProfileEntity
 
-@Database(entities = [ShlokaEntity::class] , version = 1 , exportSchema =false)
+@Database(entities = [ShlokaEntity::class , UserProfileEntity :: class , SadhanaEntryEntity:: class] , version = 2, exportSchema =false)
+@TypeConverters(Converters::class)
 abstract class GitaDatabase: RoomDatabase() {
+
     abstract fun shlokaDao() : ShlokaDao
+
+    abstract fun userProfileDao() : UserProfileDao
+
+    abstract fun sadhanaDao() : SadhanaDao
 }
