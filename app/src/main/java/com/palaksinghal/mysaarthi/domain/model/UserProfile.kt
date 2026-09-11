7bfdff4a3e0e9e20985980fb41f48462b9fa1ecf
palaksinghal14 <palaksinghal148@gmail.com>
2026-09-11 17:21:27 +0530

    feat: update userprofile model class to include location-lat & lng , lastupdatedtime and geohash value

--- app/src/main/java/com/palaksinghal/mysaarthi/domain/model/UserProfile.kt
+++ app/src/main/java/com/palaksinghal/mysaarthi/domain/model/UserProfile.kt
@@ -1,11 +1,15 @@
 package com.palaksinghal.mysaarthi.domain.model
 
+import com.google.firebase.firestore.PropertyName
+
 data class UserProfile(
     val uid: String = "",
     val displayName: String = "",
     val practices: List<String> = emptyList(),
     val howLongOnPath: String = "",
-    val isOpenToSatsang: Boolean = false,
+    @get:PropertyName("openToSatsang")
+    @set:PropertyName("openToSatsang")
+    var isOpenToSatsang: Boolean = false,
     val spiritualIntro: String = "",
     val onboardingCompleted: Boolean = false,
     val practiceReminders :List<Map<String, Any>> = emptyList(),
